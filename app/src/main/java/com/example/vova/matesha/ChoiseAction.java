29160e2709121a809face2d42c182b7d8f6d1a39
Vova <kruvonosvova@gmail.com>
2019-04-18 20:19:33 +0300

    main screan

--- app/src/main/java/com/example/vova/matesha/ChoiseAction.java
+++ app/src/main/java/com/example/vova/matesha/ChoiseAction.java
@@ -1,6 +1,8 @@
 package com.example.vova.matesha;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.PopupMenu;
@@ -12,28 +14,70 @@ import android.widget.ImageButton;
 
 import com.wajahatkarim3.easyflipview.EasyFlipView;
 
+import io.github.kexanie.library.MathView;
+
+import static android.content.pm.ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+
 public class ChoiseAction extends AppCompatActivity {
     Intent intent;
     Button learnBtn, theoriBtn, testsBtn;
+    DBHelper helper;
     EasyFlipView card;
+    MathView front, back;
     public final static String INTENT_KEY = "FROM BTN";
     public final static String SUB_KEY = "ALG/GEOM";
+    boolean side = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choise_action);
-
+        setRequestedOrientation(SCREEN_ORIENTATION_PORTRAIT);
+        helper = new DBHelper(this);
+        SQLiteDatabase db = helper.getReadableDatabase();
+        final Cursor query = db.rawQuery("SELECT shortTheory FROM learn ORDER BY RANDOM()", null);
+        front = findViewById(R.id.front_side);
+        back = findViewById(R.id.back_side);
         learnBtn = findViewById(R.id.learn);
         theoriBtn = findViewById(R.id.theori);
         testsBtn = findViewById(R.id.tests);
         card = findViewById(R.id.flip_card);
 
+        if (query.moveToFirst()) {
+            front.setText(query.getString(0));
+        }
+
+        query.moveToNext();
+        back.setText(query.getString(0));
+
         card.setOnFlipListener(new EasyFlipView.OnFlipAnimationListener() {
             @Override
             public void onViewFlipCompleted(EasyFlipView easyFlipView, EasyFlipView.FlipState newCurrentSide) {
-                Log.e("FLIP","flip");
+                if (!query.isLast()) {
+                    if (side) {
+                        side = !side;
+                        if (query.moveToNext()) {
+                            back.setText(query.getString(0));
+                        }
+                    } else {
+                        side = !side;
+                        if (query.moveToNext()) {
+                            front.setText(query.getString(0));
+                        }
+                    }
+                }
+                else if(side){
+                    query.moveToFirst();
+                    side = !side;
+                    back.setText(query.getString(0));
+                }
+                else {
+                    query.moveToFirst();
+                    side = !side;
+                    front.setText(query.getString(0));
+                }
             }
+
         });
 
         View.OnClickListener onClick = new View.OnClickListener() {
